e5b34eff2d343ff15ac4a087a3d437c4769132e8
xuwenzhe <wenzhe@umich.edu>
2017-10-10 02:36:53 -0400

    draft 1

--- js/app.js
+++ js/app.js
@@ -1,13 +1,62 @@
 /*
  * Create a list that holds all of your cards
  */
+var symbols = ["diamond","diamond","paper-plane-o","paper-plane-o","anchor","anchor","bolt","bolt","cube","cube","leaf","leaf","bicycle","bicycle","bomb","bomb"];
+var open = [];
+var moves = 0;
+var match = 0;
+var stars = 3;
+var startTime;
+
+// reset game listener
+$(".restart").click(function() {
+	initGame();
+});
+
+// initialize memory game
+function initGame() {
+	let cards = shuffle(symbols);
+	
+	$(".stars").empty();
+	for (var i = 0; i < 3; i++) {
+		let star = $('<li><i class="fa fa-star"></i></li>');
+		$(".stars").append(star);
+	}
+	$(".moves").text("0");
+	// initial global vars
+	open = [];
+	moves = 0;
+	match = 0;
+	stars = 3;
+	// activate timer
+	let date = new Date();
+	startTime = date.getTime();
+
+	// add event listeners to each card
+	// handler function : clickCard()
+	$(".deck").empty();
+	for (var i = 0; i < cards.length; i++) {
+		let card = $('<li class="card"><i class="fa fa-'+cards[i]+'"></i></li>')
+		$(".deck").append(card);
+		card.click(clickCard);
+	}
+}
+
+// When all cards are successfully flipped, 
+// confirm dialog shows up giving gamer's performance 
+// in time usage and star level.
+function endGame() {
+	let date = new Date();
+	let playAgain = confirm("Congratulations! You used " + ((date.getTime()-startTime)/1000).toFixed(2) + " seconds with " + stars + "stars. Play again?");
+	if (playAgain) initGame();
+}
 
 
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+ *   - add each card"s HTML to the page
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -32,7 +81,60 @@ function shuffle(array) {
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card"s symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+// helper function
+// compare whether two flipped cards match
+function getSymbol(card) {
+	return card.children().attr('class').substring(6);
+}
+
+// handler
+function clickCard() {
+	let currentCard = $(this);
+	currentCard.addClass('open show');
+
+	// first card in the "two" group
+	if (open.length == 0) {
+		open.push(currentCard);
+	}
+	// second card in the "two" group
+	else {
+		// timeout is 500 ms
+		setTimeout(function() {
+			moves++;
+			currentCard.removeClass('open show');
+			open[0].removeClass('open show');
+			if (getSymbol(currentCard) == getSymbol(open[0])) {
+				match++;
+				currentCard.addClass('match');
+				open[0].addClass('match');
+			}
+			open.pop();
+			$(".moves").text(moves);
+
+			if (moves == 15) {
+				$(".stars").children().last().remove();
+				stars--;
+			}
+			if (moves == 30) {
+				$(".stars").children().last().remove();
+				stars--;
+			}
+			if (moves == 45) {
+				$(".stars").children().last().remove();
+				stars--;
+			}
+			if (match == 8) {
+				endGame();
+			}
+		}, 500);
+		
+	}
+}
+
+// main function
+initGame()
